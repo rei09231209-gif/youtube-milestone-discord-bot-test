75a2d620587df698963ca70f58fa9754a14fc3e3
rei09231209-gif <rei09231209@gmail.com>
2026-01-03 17:12:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord import app_commands
 import asyncio
 import requests
 import sqlite3
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from threading import Thread
 from flask import Flask
 import os
